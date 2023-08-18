1a9f1ea3af275c7c3bfc60de2d5ae5015d334ef0
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-17 22:47:44 -0400

    Edit: Rewrite game() function
    Rewrite game() function to implicitly reinitiate playerSelection
    and computerSelection variables so that the code takes new input
    each round.

--- script.js
+++ script.js
@@ -3,26 +3,56 @@ function getComputerChoice() {
     let values = ["Rock", "Paper", "Scissors"]   
     let randomized = Math.floor(Math.random() * values.length)
     let response = values[randomized]
-    return response
-}
-
-
-function playRound(playerSelection, computerSelection) {
-    let input = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
-
-    if (input === computerSelection) {
-    return "It's a tie!"
-   } else if (input === "Rock" && computerSelection === "Paper") {
-    return "You Lose! Paper beats Rock.";
-   } else if (input === "Paper" && computerSelection === "Scissors") {
-    return "You Lose! Scissors beat Paper.";
-   } else if (input === "Scissors" && computerSelection === "Rock") {
-    return "You Lose! Rock beats Scissors."
-   } else {
-    return "You win!"
-   }
+    return response;
 }
 
 const playerSelection = prompt("Rock, Paper, or Scissors?");
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, computerSelection) {
+    let input = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
+    let humanScore = 0;
+    let computerScore = 0;
+
+    if (input === computerSelection) {
+    alert("It's a tie!")
+    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + ".";
+   } else if (input === "Rock" && computerSelection === "Paper") {
+    alert("You Lose! Paper beats Rock.")
+    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + 1 + ".";
+   } else if (input === "Paper" && computerSelection === "Scissors") {
+    alert("You Lose! Scissors beat Paper.")
+    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + 1 + ".";
+   } else if (input === "Scissors" && computerSelection === "Rock") {
+    alert("You Lose! Rock beats Scissors.")
+    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + 1 + ".";
+   } else {
+    alert("You win!")
+    return "The human has scored " + humanScore + 1 + ". The computer has scored " + computerScore + ".";
+   }
+}
+
+
+
+function game() {
+    console.log(playRound(playerSelection, computerSelection))
+}
+
+
+console.log(game())
+
+
+
+
+   /* console.log()
+    if(computerScore > playerScore) {
+        return "The machine wins!"
+    } else if(computerScore < playerScore) {
+        return "The human prevails!"
+    } else {
+        return "Mortal and machine are tied."
+    } */
+
+
+
+    
