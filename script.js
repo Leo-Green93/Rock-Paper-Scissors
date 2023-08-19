44a11c98399fcd36a5bc137f6b99b306af4a04d9
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-19 11:08:21 -0400

    edit: Create functions to calculate scores
    Instead of simply adding a point to each score within a string returned
    by playRound(), there are now two functions totalComputerScore() and
    totalHumanScore() which are meant to tally the scores after each round.

--- script.js
+++ script.js
@@ -6,53 +6,56 @@ function getComputerChoice() {
     return response;
 }
 
-const playerSelection = prompt("Rock, Paper, or Scissors?");
+function getPlayerChoice() {
+ return prompt("Rock, Paper, or Scissors?")
+}
+
 const computerSelection = getComputerChoice();
+const humanScore = 0;
+const computerScore = 0;
+
+function totalHumanScore() {
+    return humanScore + 1
+}
+
+function totalComputerScore() {
+    return computerScore + 1
+}
 
 function playRound(playerSelection, computerSelection) {
     let input = playerSelection.charAt(0).toUpperCase() + playerSelection.substr(1).toLowerCase();
-    let humanScore = 0;
-    let computerScore = 0;
-
+    
+    let computerRoundScore = totalComputerScore()
+    let humanRoundScore = totalHumanScore()
+    
     if (input === computerSelection) {
     alert("It's a tie!")
-    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + ".";
+    return "The scores have not changed.";
    } else if (input === "Rock" && computerSelection === "Paper") {
     alert("You Lose! Paper beats Rock.")
-    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + 1 + ".";
+    computerRoundScore += 1;
+    return "The human has scored " + humanRoundScore + ". The computer has scored " + computerRoundScore + ".";
    } else if (input === "Paper" && computerSelection === "Scissors") {
     alert("You Lose! Scissors beat Paper.")
-    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + 1 + ".";
+    computerRoundScore += 1;
+    return "The human has scored " + humanRoundScore + ". The computer has scored " + computerRoundScore + ".";
    } else if (input === "Scissors" && computerSelection === "Rock") {
     alert("You Lose! Rock beats Scissors.")
-    return "The human has scored " + humanScore + ". The computer has scored " + computerScore + 1 + ".";
+    computerRoundScore += 1;
+    return "The human has scored " + humanRoundScore + ". The computer has scored " + computerRoundScore + ".";
    } else {
     alert("You win!")
-    return "The human has scored " + humanScore + 1 + ". The computer has scored " + computerScore + ".";
+    humanRoundScore += 1
+    return "The human has scored " + humanRoundScore + ". The computer has scored " + computerRoundScore + ".";
    }
 }
 
-
-
 function game() {
-    console.log(playRound(playerSelection, computerSelection))
+    for(let i = 0; i < 5; i++) {
+        let newPlayerSelection = getPlayerChoice()
+        let newComputerSelection = getComputerChoice();
+        console.log(playRound(newPlayerSelection, newComputerSelection))
+}
 }
 
-
 console.log(game())
-
-
-
-
-   /* console.log()
-    if(computerScore > playerScore) {
-        return "The machine wins!"
-    } else if(computerScore < playerScore) {
-        return "The human prevails!"
-    } else {
-        return "Mortal and machine are tied."
-    } */
-
-
-
-    
